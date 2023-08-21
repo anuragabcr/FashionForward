02fb6f707cd286d324096d9616ba02e386a82c7e
Anurag Kumar <anuragabcr@gmail.com>
2023-08-21 22:42:25 +0530

    products added

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,25 @@
-const HomePage = () => {
-  return <div>HomePage</div>;
+import { getBillboard } from "@/actions/billboard";
+import { getProducts } from "@/actions/products";
+import Billboard from "@/components/Billboard";
+import Container from "@/components/Container";
+import ProductList from "@/components/ProductList";
+
+const HomePage = async () => {
+  const billboard = await getBillboard("913fb373-d9cb-41b3-8232-3f9ef36b2a82");
+
+  const products = await getProducts({ isFeatured: true });
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
---
+++ components/NoResults.tsx
@@ -0,0 +1,9 @@
+const NoResults = () => {
+  return (
+    <div className="flex items-center justify-center h-full text-neutral-500">
+      No Results Founds
+    </div>
+  );
+};
+
+export default NoResults;
